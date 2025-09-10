23a7e2724995c8c61bbbb480c7ebcbb43db39a8a
dwidge <dwidge+gh@gmail.com>
2025-09-10 12:32:26 +0200

    fix(useDeviceId): Regenerate id if invalid or missing

--- src/useDevice.ts
+++ src/useDevice.ts
@@ -3,11 +3,19 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 import useSWR from "swr";
+import z from "zod";
 import { randId } from "./randId.js";
 
-export const useDeviceId = (): string | undefined => {
-  const h = useSWR<string, Error>(() => "deviceId");
-  if (!h.isLoading && !h.isValidating && h.data === undefined)
-    h.mutate(randId(12));
-  return h.data;
+export const useDeviceId = (key = "deviceId"): string | undefined => {
+  const h = useSWR<string, Error>(() => key);
+  let r = h.data;
+  if (
+    !h.isLoading &&
+    !h.isValidating &&
+    (h.data === undefined || z.ostring().safeParse(h.data).success === false)
+  ) {
+    r = z.string().parse(randId(12));
+    h.mutate(r);
+  }
+  return z.ostring().parse(r);
 };
